cec29102a4c3d2a36111488cbad36ee7b33d8c0d
ztatro <57099041+ztatro@users.noreply.github.com>
2021-03-03 12:08:52 -0500

    Update main.js
    added player to the main file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import * as randomWalker from "./randomWalker.js";
 import * as wall from "./wall.js";
 import * as utils from "./utils.js";
+import * as player from "./player_class.js";
 
 function init(){
 }
